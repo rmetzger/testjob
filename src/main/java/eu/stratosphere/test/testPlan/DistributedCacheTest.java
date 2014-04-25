5b88588a7d6b0cc08a69566bf869a8715c349ed5
Robert Metzger <metzgerr@web.de>
2014-04-25 17:04:50 +0200

    newer version

--- src/main/java/eu/stratosphere/test/testPlan/DistributedCacheTest.java
+++ src/main/java/eu/stratosphere/test/testPlan/DistributedCacheTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import eu.stratosphere.api.common.JobExecutionResult;
 import eu.stratosphere.api.common.Plan;
+import eu.stratosphere.api.common.Program;
 import eu.stratosphere.api.common.cache.DistributedCache;
 import eu.stratosphere.api.common.operators.FileDataSink;
 import eu.stratosphere.api.common.operators.FileDataSource;
@@ -26,7 +27,7 @@ import eu.stratosphere.util.Collector;
  * @author robert
  *
  */
-public class DistributedCacheTest {
+public class DistributedCacheTest implements Program  {
 	private static final long serialVersionUID = 1L;
 
 	public static class PoorJoin extends MapFunction {
